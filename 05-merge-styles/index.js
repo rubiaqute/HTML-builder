95cfb896829ca0b8495f839a65699f98602dbff9
rubiaqute <rubiaqute@gmail.com>
2021-10-31 00:55:38 +0400

    feat:add 5 task solution

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+let stylesArray=[];
+const pathStyles = path.resolve('05-merge-styles', 'styles')
+const bundlePath = path.resolve('05-merge-styles', 'project-dist', 'bundle.css')
+fs.readdir(pathStyles,{withFileTypes:true},(err,files)=>{
+    if(err) console.log(err)
+    else{
+        
+    files.forEach((file)=>{
+        
+        const pathText = path.resolve(pathStyles, file.name)
+        const fileExtract =  returnExtract(file, pathText).slice(1)
+        if (fileExtract=="css"){
+        const stream = fs.createReadStream(pathText, 'utf-8');
+        let text="";
+        stream.on('data', partData => text += partData);
+        stream.on('error', error => console.log('Error', error.message))
+        stream.on('end', ()=> {
+            stylesArray.push(text.trim())
+            fs.open (bundlePath, 'w', (err) => {if (err) throw err} )
+            fs.appendFile(bundlePath, text.trim(), function(){})
+            
+        })
+    }
+    
+    })
+    
+}
+})
+function returnExtract(file, pathText){
+    return path.extname(path.resolve(pathText,file.name))
+}
